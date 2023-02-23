84277e009abca7827181d519be1c215d7427848b
saber <saber.tabatabaee@gmail.com>
2023-02-23 22:58:44 +0330

    profile image and logo in main admin panel page

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -1,6 +1,6 @@
     <!-- Brand Logo -->
     <a href="{{route('home')}}" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
       </a>
@@ -10,7 +10,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="{{Auth()->user()??Auth()->user()->avatar}}" class="img-circle elevation-2" alt="User Image">
+            <img src="{{!Auth()->user()?"":Auth()->user()->avatar}}" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="{{route('users.profile',Auth()->user()->id)}}" class="d-block">{{Auth()->user()->name}}</a>
